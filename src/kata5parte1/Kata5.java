b65beb2c742e0ad985a58443d02f97f3e1b0a6fe
Elena Navarro <99991881+ElenaaNavarroo@users.noreply.github.com>
2022-11-27 21:14:46 +0000

    tercera version de la kata 5 parte 1
    Se ha cambiado un poco la base de datos para poder trabajar mejor.

--- src/kata5parte1/Kata5.java
+++ src/kata5parte1/Kata5.java
@@ -1,11 +1,21 @@
 package kata5parte1;
 
+import java.util.List;
+
 public class Kata5 {
-
+    
     public static void main(String[] args) { 
-        SelectApp app = new SelectApp(); 
-        app.selectAll();
-
+        //SelectApp app = new SelectApp(); 
+        //app.selectAll();
+        
+        //CrearTabla crearTablaMail = new CrearTabla();  
+        //crearTablaMail.createNewTable();
+        
+        List<String> mailList;
+        String fileName = new String("email.txt");
+        mailList = MailListReader.read(fileName);
+        InsertarDatosTabla insertarDatosTabla = new InsertarDatosTabla();
+        insertarDatosTabla.insert(mailList);
     }
 
 }
