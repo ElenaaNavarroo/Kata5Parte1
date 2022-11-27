406a03392c9eb3c9f7e98160b715a5fe9c7cdde2
Elena Navarro <99991881+ElenaaNavarroo@users.noreply.github.com>
2022-11-27 20:57:33 +0000

    Segunda version kata 5 parte 1

--- src/kata5parte1/SelectApp.java
+++ src/kata5parte1/SelectApp.java
@@ -7,10 +7,8 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class SelectApp {
-    // Se conecta a la BD y se devuelve un objeto Connection 
 
     private Connection connect() {
-        // SQLite connection string
         String url = "jdbc:sqlite:KATA5.db"; 
         Connection conn = null;
         try {
@@ -21,14 +19,11 @@ public class SelectApp {
         return conn;
     }
 
-
-    // Se seleccionan todos los registros de la tabla PEOPLE 
     public void selectAll(){
         String sql = "SELECT * FROM PEOPLE";
         try (Connection conn = this.connect(); 
                 Statement stmt = conn.createStatement(); 
                 ResultSet rs = stmt.executeQuery(sql)){
-                // Bucle sobre el conjunto de registros.
                 while (rs.next()) { 
                     System.out.println(rs.getInt("id") + "\t" +
                                        rs.getString("Name") + "\t" + 
